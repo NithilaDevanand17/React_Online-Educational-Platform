b977b579d24b7ceaaf0c569d1a0d71e5534d0ba6
NithilaDevanand17 <114006106+NithilaDevanand17@users.noreply.github.com>
2023-03-29 15:31:45 +0530

    Add files via upload

---
+++ dummydata.js
@@ -0,0 +1,359 @@
+export const homeAbout = [
+  {
+    id: 1,
+    cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
+    title: "Flexible learning",
+    desc: "Explore any interest or trending topic,move between multiple courses, or switch to a different course"
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/diploma.png",
+    title: "Earn A Certificates",
+    desc: "Earn a certificate for every learning program that you complete at no additional cost"
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/athlete.png",
+    title: "Save money",
+    desc: "Spend less money on your learning if you plan to take multiple courses this year"
+  },
+]
+export const awrapper = [
+  {
+    cover: "https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/80/ffffff/external-graduation-education-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png",
+    data: "3,000",
+    title: "SUCCESS STORIES",
+  },
+
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/athlete.png",
+    data: "320",
+    title: "TRUSTED TUTORS",
+  },
+  {
+    cover: "https://img.icons8.com/external-outline-icons-maxicons/80/ffffff/external-calender-insurance-outline-outline-icons-maxicons.png",
+    data: "1,000",
+    title: "SCHEDULES",
+  },
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/macbook-idea--v3.png",
+    data: "587",
+    title: "COURSES",
+  },
+]
+export const coursesCard = [
+  {
+    id: 1,
+    cover: "../images/courses/c1.png",
+    coursesName: "Machine Learning in Python",
+    
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Prakash Mohan",
+        totalTime: "50 lectures (190 hrs)",
+      },
+    ],
+    priceAll: "2000 All Course",
+    pricePer: "780 per month",
+  },
+  {
+    id: 2,
+    cover: "../images/courses/c2.png",
+    coursesName: "Basics in R programming",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Ram Gurung",
+        totalTime: "30 lectures (125 hrs)",
+      },
+    ],
+    priceAll: "2000 All Course",
+    pricePer: "250 per month",
+  },
+  {
+    id: 3,
+    cover: "../images/courses/c3.png",
+    coursesName: "Deep Learning Concepts",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Sathya",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "5000 All Course",
+    pricePer: "500 per month",
+  },
+  {
+    id: 4,
+    cover: "../images/courses/c4.png",
+    coursesName: "HTML, CSS, and Javascript for Web Developers",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Saroj Nepal",
+        totalTime: "20 lectures (20 hrs)",
+      },
+    ],
+    priceAll: "3444 All Course",
+    pricePer: "383 per month",
+  },
+  {
+    id: 5,
+    cover: "../images/courses/c5.png",
+    coursesName: "Introducing to Programming with ReactJS",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Suriya",
+        totalTime: "100 lectures (150 hrs)",
+      },
+    ],
+    priceAll: "3000 All Course",
+    pricePer: "300 per month",
+  },
+  {
+    id: 6,
+    cover: "../images/courses/c6.png",
+    coursesName: "Speech and Language Processing",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Surenther",
+        totalTime: "200 lectures (300 hrs)",
+      },
+    ],
+    priceAll: "5300 All Course",
+    pricePer: "800 per month",
+  },
+  {
+    id: 7,
+    cover: "../images/courses/c7.png",
+    coursesName: "Database Management System",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by petter",
+        totalTime: "20 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "2600 All Course",
+    pricePer: "200 per month",
+  },
+  {
+    id: 8,
+    cover: "../images/courses/c8.png",
+    coursesName: "Fundamentals of Data Science basics",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Kavitha",
+        totalTime: "80 lectures (200 hrs)",
+      },
+    ],
+    priceAll: "8300 All Course",
+    pricePer: "400 per month",
+  },
+  {
+    id: 9,
+    cover: "../images/courses/c9.png",
+    coursesName: "Statistical Learning and its Application",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Darshini",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "5800 All Course",
+    pricePer: "300 per month",
+  },
+]
+export const online = [
+  {
+    cover: "./images/courses/online/o1.png",
+    hoverCover: "./images/courses/online/o1.1.png",
+    courseName: "UI/UX Design Courses",
+    course: "25 Courses",
+  },
+   
+  
+  
+   
+  {
+    cover: "./images/courses/online/o6.png",
+    hoverCover: "./images/courses/online/o6.1.png",
+    courseName: "Information Software",
+    course: "60 Courses",
+  },
+   
+  {
+    cover: "./images/courses/online/o8.png",
+    hoverCover: "./images/courses/online/o8.1.png",
+    courseName: "Artificial Intelligence",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o9.png",
+    hoverCover: "./images/courses/online/o9.1.png",
+    courseName: "Graphic Design",
+    course: "80 Courses",
+  },
+  {
+    cover: "./images/courses/online/o11.png",
+    hoverCover: "./images/courses/online/o11.1.png",
+    courseName: "Business Administration",
+    course: "17 Courses",
+  },
+  {
+    cover: "./images/courses/online/o12.png",
+    hoverCover: "./images/courses/online/o12.1.png",
+    courseName: "Web Management",
+    course: "17 Courses",
+  },
+]
+// export const team = [
+//   {
+//     cover: "./images/team/t1.webp",
+//     name: "Ph.D Adrian Molises",
+//     work: "DEVELOPER AND LEAD INSTRUCTOR",
+//   },
+//   {
+//     cover: "./images/team/t2.webp",
+//     name: "Ph.D Arthur MaGregor",
+//     work: "DEVELOPER AND LEAD INSTRUCTOR",
+//   },
+//   {
+//     cover: "./images/team/t2.webp",
+//     name: "Ph.D Arthur MaGregor",
+//     work: "DEVELOPER AND LEAD INSTRUCTOR",
+//   },
+//   {
+//     cover: "./images/team/t2.webp",
+//     name: "Ph.D Arthur MaGregor",
+//     work: "DEVELOPER AND LEAD INSTRUCTOR",
+//   },
+  
+  
+
+export const price = [
+  {
+    name: "Single learning program",
+    price: "₹4,051/Month",
+    desc: "Learn a single topic or skill and earn a credential,Visit an individual course or Specialization page to purchase.",
+  },
+  {
+    name: "Monthly Package",
+    price: "₹4,878/Month",
+    desc: "Complete multiple courses and earn credentials in the short term, start 7 day free trial and cancel anytime.",
+  },
+  {
+    name: "Annual package",
+    price: "₹32,994/Year",
+    desc: "Combine flexibility and savings with long-term learning goals,Enjoy maximum flexibility to achieve work/life balance and learn at your own pace",
+  },
+   
+]
+export const faq = [
+  {
+    title: "Can I try this , to make sure its righ for me? ",
+    desc: "Yes! We have two options, depending on which payment plan you choose.If you opt to make an annual payment, you'll have up to 14 days to submit a refund request if you decide that Coursera Plus isn't right for you. The process is simple—just go to your My Purchases page within 14 days of your payment and submit a refund request. No additional information is required. Please note that any certificates that you earn within your first 14 days will be revoked if you decide to get a refund within that period." 
+  },
+  {
+    title: "What is included in subscription?",
+    desc: "Wuth your subscription, you get unlimited access to more than 7,000 courses, Projects, Specializations, and Professional Certificate programs in a variety of domains, including data science, business, computer science, health, personal development, humanities, and more." 
+  },
+  {
+    title: "Will I save money with my premium? ",
+    desc: "Yes! If you're taking more than one course regularly, you can save up to 30% each month. The more you learn, the more you save!" 
+  },
+  {
+    title: "How many certifications I can earn with this premium?",
+    desc: "There is no limit! You will earn a Certificate for every course, Project, Specialization, or Professional Certificate that you complete." 
+  },
+  {
+    title: "Can I get reimbursed by my employer?",
+    desc: "Yes, if your employer offers educational reimbursements that cover online learning programs. You will receive an email with your receipt that you can submit to your employer to get reimbursed for your subscription." 
+  },
+  
+]
+export const blog = [
+  {
+    id: 1,
+    type: "admin",
+    date: "JAN. 18, 2021",
+    com: "3 COMMENTS ",
+    title: "How Chat GPT Works",
+    desc: "ChatGPT is an extrapolation of a class of machine learning Natural Language Processing models known as Large Language Model.A brief introduction to the intuition and methodology behind the chat GPT you cant stop hearing about.",
+    cover: "../images/blog/b1.webp",
+  },
+  {
+    id: 2,
+    type: "admin",
+    date: "API. 25, 2022",
+    com: "5 COMMENTS ",
+    title: "The Alternative to Performance Reviews for Software Engineers",
+    desc: "With the increasing reliance on software in every aspect of our lives, using management techniques from traditional industries in software engineering teams has gained momentum.",
+    cover: "../images/blog/b2.webp",
+  },
+  {
+    id: 3,
+    type: "user",
+    date: "MAY. 15, 2022",
+    com: "10 COMMENTS ",
+    title: "Electronic Skin Change",
+    desc: "In recent years, I have seen an uptick in research on electronic skin or e-skin. E-skin is worn over human skin and can be used to garner information about bodily conditions, such as heart rate, blood pressure, and other biometrics.",
+    cover: "../images/blog/b3.webp",
+  },
+  {
+    id: 4,
+    type: "admin",
+    date: "JAN. 02, 2022",
+    com: "3 COMMENTS ",
+    title: "AI in Space",
+    desc: "Artificial intelligence and robotics are helping humans solve problems faster. Robots are machines that have inbuilt artificial intelligence. They are used in all kinds of situations, especially where they can alleviate strenuous tasks or complete missions that are too dangerous for a human to undertake.",
+    cover: "../images/blog/b4.webp",
+  },
+  {
+    id: 5,
+    type: "admin",
+    date: "DEC. 14, 2022",
+    com: "3 COMMENTS ",
+    title: "Coding Wont Exist In 5 Years. This Is Why",
+    desc: "Coding, which was once the backbone of the digital world, will become obsolete soon.AI-powered tools are ready to take over, and traditional ways of writing code will become less popular.The tech industry is shifting, and the race is on to adapt or be left behind.",
+    cover: "../images/blog/b5.webp",
+  },
+  {
+    id: 6,
+    type: "user",
+    date: "JAN. 18, 2021",
+    com: "12 COMMENTS ",
+    title: "Cloud future overcast by security concerns ",
+    desc: "Cloud services are crucial elements of many business processes. Cloud computing allows businesses to reduce costs, accelerate deployments, develop at scale, share files easily and collaborate efficiently without needing a centralized location.",
+    cover: "../images/blog/b6.webp",
+  },
+]
+export const testimonal = [
+  {
+    id: 1,
+    name: "Avanthika",
+    post: "Data Analyst",
+    desc: "I learned business statistics and analysis to apply to my job,but my DSAC subscription motivated me to keep learning, now I have a new career goal to pivot into data analyst.",
+    cover: "./images/testo/t1.webp",
+  },
+  {
+    id: 2,
+    name: "Vishnu",
+    post: "Fullstack Developer",
+    desc: "DSAC keeps me motivated to learn.With each courses i am getting more value out of my subscription.I can focus more on front end and back end related certificates to achieve more knowledge. ",
+    cover: "./images/testo/t2.webp",
+  },
+  {
+    id: 3,
+    name: "Nithish",
+    post: "Data Scientist",
+    desc: "I can try my course and switch to another one for no additional cost. This motivates me to learn even more at one point I was thinking three courses related to data science at the same time! ",
+    cover: "./images/testo/t3.webp",
+  },
+]
